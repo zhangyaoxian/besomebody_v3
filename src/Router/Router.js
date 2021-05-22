62b36aa6cff0e39d60073b826a391ef6c125a5f2
zhangyaoxian <78006707+zhangyaoxian@users.noreply.github.com>
2021-05-22 11:16:52 +0800

    Added mcq minigame

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -16,6 +16,7 @@ import HomePage from '../pages/HomePage/HomePage'
 import CharProfilePage from '../pages/CharProfilePage/CharProfilePage'
 import CharacterChoice from "../pages/HomePage/CharacterChoice"
 
+import MultipleChoiceQuiz from '../pages/MiniGames/MultipleChoiceQuiz/MultipleChoiceQuiz/MultipleChoiceQuiz'
 // import InkTestPage from '../pages/InkTestPage/InkTestPage'
 // import InkTestPageBasic from '../pages/InkTestPage/InkTestPageBasic'
 // import InstagramPage from '../pages/InstagramPage/InstagramPage'
@@ -72,6 +73,10 @@ class Router extends Component {
 
           <Route path="/profile" exact>
             <CharProfilePage />
+          </Route>
+
+          <Route path="/quiz" exact>
+            <MultipleChoiceQuiz />
           </Route>
 
           
